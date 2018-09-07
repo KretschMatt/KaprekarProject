b69ad1d575a2bfbf9f290ef64ff38383c8103634
Matt Kretsch <kretsch.matt@gmail.com>
2018-09-07 17:06:20 -0500

    changes
    line 45
      modulus spelt wrong

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -42,7 +42,7 @@ double numberOfDigits, ;
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
