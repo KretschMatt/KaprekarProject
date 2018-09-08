92e45cd37f7b943ba177177b60736a6f2f0d7b9b
Matt Kretsch <kretsch.matt@gmail.com>
2018-09-07 19:28:28 -0500

    big change*
    changed log10 to pow, log does not look correct. Come back if needed

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -34,7 +34,7 @@ int numberOfDigits;
   //compute the square
   long square = n * (long) n;
   //compute the number of digits of the square
-  numberOfDigits = (int) log10(n) + 1;
+  numberOfDigits = (int) pow(10, n);
   long modulus;
   modulus = 1;
   long first, second;
